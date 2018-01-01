90ca1abe3ed709e45b0903288f684aa2a59f5c9a
coolworld2015 <wintermute2015@ukr.net>
2018-01-01 19:39:51 +0200

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import React, {Component} from 'react';
+
+import {AppRegistry} from 'react-native';
+
+import App from './app/src/android/app/app';
+
+AppRegistry.registerComponent('rnGAI', () => App);
